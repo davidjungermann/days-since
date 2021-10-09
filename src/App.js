b95e2787a83107e64e15c2fadcafd67524e71e36
davidjungermann <david.jungermann@gmail.com>
2021-10-09 14:57:52 +0200

    Basic layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import './App.css';
+import "./App.css";
+import Emoji from "./components/emoji/Emoji";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Days Since</h1>
-      </header>
+      <div className="title">
+        <h1 className="heading">Days Since 🔥</h1>
+        <Emoji symbol="" fontSize={50} />
+      </div>
+      <h1 className="counter">{counter}</h1>
+      <div className="increase-decrease">
+        <div className="check" onClick={() => setCounter(counter + 1)}>
+          <Emoji symbol="✅" fontSize={75} />
+        </div>
+        <div className="cross" onClick={() => setCounter(0)}>
+          <Emoji symbol="❌" fontSize={75} />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
