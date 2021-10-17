55a96482584c8354e75f9987720cb74eeafe0755
davidjungermann <david.jungermann@gmail.com>
2021-10-17 20:34:35 +0200

    Outlines login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Counter from "./pages/counter/Counter";
+import Login from "./pages/login/Login";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-      <Counter />
+      <Router>
+        <Switch>
+          <Route path="/" component={Login} />
+          <Route path="/counter" component={Counter} />
+        </Switch>
+      </Router>
     </div>
   );
 };
---
+++ src/auth/authServices.js
@@ -0,0 +1,45 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { auth } from "../db/firestore";
+
+const createUser = (email, password) => {
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+    });
+};
+
+const signInUser = (email, password) => {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+};
+
+const signOutUser = (email, password) => {
+  signOut(auth)
+    .then(() => {
+      // Sign-out successful.
+    })
+    .catch((error) => {
+      // An error happened.
+    });
+};
+
+export { createUser, signInUser, signOutUser };
