7d669c14d9139dd65816b614f899decb07825c09
davidjungermann <david.jungermann@gmail.com>
2022-01-13 20:33:04 +0100

    Adds cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,13 @@
 const functions = require('firebase-functions');
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
-});
-
-//test
+exports.scheduledFunctionCrontab = functions
+  .region('europe-west1')
+  .pubsub.schedule('*/5 * * * *')
+  .timeZone('Europe/Stockholm')
+  .onRun(async (context) => {
+    console.log('This will be run every five minutes');
+    const writeResult = await admin
+      .firestore()
+      .collection('support')
+      .add({ batman: 'David', robin: 'Mansour' });
+  });
