556730e1b16c7272991dfb5d02046b0bb9134292
davidjungermann <david.jungermann@gmail.com>
2022-01-14 14:16:47 +0100

    Updates credential usage

--- src/db/firestore.js
+++ src/db/firestore.js
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth, browserLocalPersistence } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD3JgRl5KWKzWo-tVXoMqKc5HZzoNr8r5Q',
-  authDomain: 'days-since-f2f53.firebaseapp.com',
-  projectId: 'days-since-f2f53',
-  storageBucket: 'days-since-f2f53.appspot.com',
-  messagingSenderId: '770516759412',
-  appId: '1:770516759412:web:6a858719defadfc696f221',
-  measurementId: 'G-EDN478SNWY',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
